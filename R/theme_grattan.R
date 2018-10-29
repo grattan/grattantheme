04d53f5b274b94bbbbe43001e634c1b2229555cd
Matt Cowgill <mattcowgill@gmail.com>
2018-10-30 08:09:50 +1100

    turned legend off by default

--- R/theme_grattan.R
+++ R/theme_grattan.R
@@ -4,7 +4,8 @@
 #' @param base_family Font family for text elements. Defaults to "sans", indistinguishable from Arial.
 #' @examples
 #'
-#' # This function goes most of the way to making your charts Grattan-y, but manual tweaking will almost definitely be required
+#' # This function goes most of the way to making your charts Grattan-y,
+#' # but manual tweaking will almost definitely be required
 #'
 #' # A minimal example:
 #' library(ggplot2)
@@ -14,11 +15,13 @@
 #'     theme_grattan()
 #' p
 #'
-#' # To ensure your x-axis is at zero (or some other value you choose), you'll need to manually tweak the scale of the y-axis, as:
+#' # To ensure your x-axis is at zero (or some other value you choose),
+#' # you may need to manually tweak the scale of the y-axis, like this:
 #'
 #' p <- ggplot(mtcars, aes(x = wt, y = mpg)) +
 #'     geom_point() +
-#'     scale_y_continuous(limits = c(0, 35), expand = c(0,0)) +
+#'     scale_y_continuous(limits = c(0, 35),
+#'                        expand = c(0,0)) +
 #'     theme_grattan()
 #' p
 #'
@@ -31,14 +34,15 @@
 #'     theme_grattan()
 #' p
 #'
-#' # For Grattan-y charts you'll generally want to disable the legend, which is on by default. Here's how:
+#' # The legend is off by default. You may wish to turn it on. Here's how:
 #'
 #' p <- ggplot(mtcars, aes(x = wt, y = mpg, col = factor(cyl))) +
 #'     geom_point() +
 #'     scale_y_continuous(limits = c(0, 35), expand = c(0,0)) +
 #'     scale_colour_manual(values = grattan_pal(n = 3)) +
 #'     theme_grattan() +
-#'     theme(legend.position = "none")
+#'     theme(legend.position = "bottom")
+#'
 #' p
 #'
 #'
@@ -60,8 +64,16 @@ theme_grattan <- function (base_size = 18, base_family = "sans") {
      text = element_text(family = base_family,
                          colour = "black",
                          size = base_size),
-     panel.grid.major = element_line(colour = grattan_gridlinegrey),
+     panel.grid.major = element_line(),
      panel.grid.major.x = element_blank(),
+     legend.background = element_blank(),
+     legend.position = "none",
+     legend.direction = "horizontal",
+     legend.box = "vertical",
+     legend.key.height=unit(1, "line"),
+     legend.margin = margin(t=0, r=0, b=0, l=0, unit="cm"),
+     legend.spacing = unit(0,"cm"),
+     legend.title = element_blank(),
      axis.title = element_text(size = rel(1)),
      axis.text = element_text(size = rel(1)),
      axis.title.y = element_blank(),
@@ -69,14 +81,7 @@ theme_grattan <- function (base_size = 18, base_family = "sans") {
      axis.ticks.x = element_line(colour = "black"),
      axis.line.y = element_blank(),
      axis.line.x = element_line(colour = "black"),
-     legend.background = element_blank(),
-     legend.position = "bottom",
-     legend.direction = "horizontal",
-     legend.box = "vertical",
-     legend.key.height=unit(1, "line"),
-     legend.margin = margin(t=0, r=0, b=0, l=0, unit="cm"),
-     legend.spacing = unit(0,"cm"),
-     legend.title = element_blank(),
+     axis.line.x.bottom = element_line(),
      panel.grid = element_line(colour = NULL),
      panel.grid.minor = element_blank(),
      plot.title = element_text(face = "bold",
@@ -93,5 +98,5 @@ theme_grattan <- function (base_size = 18, base_family = "sans") {
                                  hjust = 0,
                                  colour = "black",
                                  face = "italic",
-                                 margin = margin(t = 15))))
+                                 margin = margin(t = 15))) )
 }
