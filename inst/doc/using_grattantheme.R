853844e751d581163484ecc0efeec37e743283b7
wfmackey <wfmackey@gmail.com>
2019-01-18 19:39:16 +1100

    add vignette and background = "box"
    Adds vignettes and fixes #14

--- inst/doc/using_grattantheme.R
+++ inst/doc/using_grattantheme.R
@@ -1,5 +1,5 @@
 ## ---- echo = FALSE, message = FALSE--------------------------------------
-knitr::opts_chunk$set(collapse = T, comment = "#>")
+knitr::opts_chunk$set(collapse = T, comment = "#>", fig.align='center')
 library(ggplot2)
 library(grattantheme)
 
@@ -11,12 +11,13 @@ library(grattantheme)
 plot <- ggplot(Orange,
                aes(x = age,
                    y = circumference,
-                   colour = Tree)) +
-        geom_line() +
+                   fill = Tree)) +
+        geom_bar(stat = "identity") +
         labs(x = "Age of tree",
-             y = "")
+             y = "",
+             colour = "Tree")
 
-## ---- echo = FALSE-------------------------------------------------------
+## ---- fig.align='center', echo = FALSE-----------------------------------
 plot
 
 ## ------------------------------------------------------------------------
@@ -30,10 +31,54 @@ plot +
 ## ------------------------------------------------------------------------
 plot + 
   theme_grattan() +
-  grattan_colour_manual(n = 5)
+  scale_y_continuous_grattan()
+
+## ------------------------------------------------------------------------
+plot + 
+  theme_grattan(background = "orange") +
+  scale_y_continuous_grattan()
+
+## ------------------------------------------------------------------------
+plot + 
+  coord_flip() +
+  theme_grattan(flipped = TRUE) +
+  scale_y_continuous_grattan()
+
+## ------------------------------------------------------------------------
+
+plot + 
+  theme_grattan(base_size = 8, base_family = "serif") +
+  scale_y_continuous_grattan()
+
+
+## ---- out.width = "250px", echo = FALSE----------------------------------
+knitr::include_graphics("../man/grattan_pal.pdf")
+
+## ------------------------------------------------------------------------
+plot + 
+  geom_vline(xintercept = 850, colour = grattan_grey3) +
+  theme_grattan() +
+  scale_y_continuous_grattan()
+
 
 ## ------------------------------------------------------------------------
 plot + 
   theme_grattan() +
-  grattan_colour_manual(n = 5, reverse = TRUE)
+  scale_y_continuous_grattan() +
+  grattan_fill_manual(n = 5)
+
+
+## ------------------------------------------------------------------------
+plot + 
+  theme_grattan() +
+  scale_y_continuous_grattan() +
+  grattan_fill_manual(n = 5, reverse = TRUE)
+
+
+## ---- error = TRUE-------------------------------------------------------
+plot + 
+  theme_grattan() +
+  scale_y_continuous_grattan() +
+  grattan_fill_manual(n = 3)
+
 
