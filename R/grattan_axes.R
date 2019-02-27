efb8a3b1912c39724e185e35e025aa3aa555e922
Matt Cowgill <mattcowgill@gmail.com>
2019-02-27 15:08:58 +1100

    add grattan_x_continuous() and scale_x_continuous_grattan()

--- R/grattan_axes.R
+++ R/grattan_axes.R
@@ -1,9 +1,10 @@
 #' Ensure your y- and x-axis are Grattan style guide-consistent and doesn't hang off the edge of the chart.
 #'
-#' @param ... arguments passed to grattan_y_continuous
 #' @param expand_bottom default is 0. This will ensure that your x-axis is at the bottom value of your plotted data. Increase to add some buffer between the lowest point in your data and the x-axis. Note that the value is interpreted as a fraction of the total plotting space - a value of 1 will add white space equal to the whole area of your data.
 #' @param expand_top default is 0.015. This will ensure that a small amount of white space is added to the top of your chart. Increase to add more white space.
-#'
+#' @param expand_left default is 0. This will ensure your y-axis is at the lowest value of your plotted value.
+#' @param expand_right default is 0.015. This will ensure that a small amount of white space is added to the right of your chart.
+#' @param ... arguments passed to scale_y_continuous or scale_x_continuous
 #' @examples
 #'
 #' # Here's a basic chart in the Grattan style:
@@ -51,19 +52,32 @@
 NULL
 
 #' @rdname grattan_axes
-#' @import ggplot2
+#' @importFrom ggplot2 scale_y_continuous expand_scale
 #' @export
 
-grattan_y_continuous <- function(..., expand_bottom = 0, expand_top = 0.015) {
+grattan_y_continuous <- function(expand_bottom = 0, expand_top = 0.015, ...) {
   ggplot2::scale_y_continuous(expand = expand_scale(mult = c(expand_bottom, expand_top)), ...)
 }
 
 
 #' @rdname grattan_axes
-#' @import ggplot2
 #' @export
 
-scale_y_continuous_grattan <- function(..., expand_bottom = 0, expand_top = 0.015) {
-  ggplot2::scale_y_continuous(expand = expand_scale(mult = c(expand_bottom, expand_top)), ...)
+scale_y_continuous_grattan <- function(expand_bottom = 0, expand_top = 0.015, ...) {
+  grattan_y_continuous(expand_bottom = expand_bottom, expand_top = expand_top, ...)
 }
 
+#' @rdname grattan_axes
+#' @importFrom ggplot2 scale_y_continuous expand_scale
+#' @export
+
+grattan_x_continuous <- function(expand_left = 0, expand_right = 0.015, ...) {
+  ggplot2::scale_x_continuous(expand = expand_scale(mult = c(expand_left, expand_right)), ...)
+}
+
+#' @rdname grattan_axes
+#' @export
+
+scale_x_continuous_grattan <- function(expand_left = 0, expand_right = 0.015, ...){
+  grattan_x_continuous(expand_left = expand_left, expand_right = expand_right, ...)
+}
