46611c967450d0f8caeaba686cc1f1fd8654ad32
Matt Cowgill <mattcowgill@gmail.com>
2019-02-27 13:52:49 +1100

    add more colours

--- R/grattan_pal.R
+++ R/grattan_pal.R
@@ -1,8 +1,10 @@
 #' Create a Grattan-appropriate palette for your chart.
 #'
-#' @param n Numeric. The number of levels in your colour scale. Minimum value is 1, maximum is 7.
-#' If you don't specify `n`, a five-colour palette will be used, which may not look right.
-#' Specify `n`. By default, n = 2 will give you light orange and dark orange.
+#' @param n Numeric. The number of levels in your colour scale. Minimum value is 1,
+#' maximum is 10. Using more than 6 is not recommended. If you don't specify `n`,
+#' a five-colour palette will be used, which may not look right. Specify `n`.
+#'
+#' By default, n = 2 will give you light orange and dark orange.
 #' Use n = "2a" if you want light orange and red.
 #' @param reverse Logical. FALSE by default. Setting to TRUE reverses the
 #' standard colour order. Standard colour order runs from light to dark. If you
@@ -10,16 +12,41 @@
 #'
 #' @examples
 #' library(ggplot2)
+#'
 #' p <- ggplot(mtcars, aes(x = wt, y = mpg, col = factor(cyl))) +
 #'     geom_point() +
 #'     theme_grattan() +
-#'     scale_colour_manual(values = grattan_pal(n = 3, reverse = FALSE))
+#'     scale_colour_manual(values = grattan_pal(n = 3))
+#'
+#' p
+#'
+#' # Alternatively, use grattan_colour_manual(), which is a wrapper
+#' # around scale_colour_manual():
+#'
+#' p <- ggplot(mtcars, aes(x = wt, y = mpg, col = factor(cyl))) +
+#'     geom_point() +
+#'     theme_grattan() +
+#'     grattan_colour_manual(n = 3)
 #'
 #' p
 #'
 #' @export
 
 grattan_pal <- function(n = 0, reverse = FALSE){
+
+  if(n == 0){
+    n <- 5
+    "Your chart will probably look better if you specify n in grattan_pal()."
+  }
+
+  if(n > 6 & n <= 10){
+    warning("Using more than six colours is not recommended.")
+  }
+
+  if(n > 10){
+    stop(paste0("You've requested ", n, " colours; grattan_pal() only supports up to 10."))
+  }
+
   if(n == 1){
     palette <- grattantheme::grattan_lightorange
   } else if(n == 2){
@@ -44,18 +71,28 @@ grattan_pal <- function(n = 0, reverse = FALSE){
     palette <- c(grattantheme::grattan_lightyellow, grattantheme::grattan_yellow,
                  grattantheme::grattan_lightorange, grattantheme::grattan_darkorange,
                  grattantheme::grattan_red, grattantheme::grattan_darkred, "black")
-    warning("Are you sure you want to use more than six colours?")
-  } else if(n == 0){
-    palette <- c(grattantheme::grattan_yellow, grattantheme::grattan_lightorange,
-                 grattantheme::grattan_darkorange, grattantheme::grattan_red,
-                 grattantheme::grattan_darkred)
-    warning("If you have fewer than 5 colours, your chart will look better if you specify n in grattan_pal().")
+  } else if(n == 8){
+    palette <- c(grattantheme::grattan_lightyellow, grattantheme::grattan_yellow,
+                 grattantheme::grattan_lightorange, grattantheme::grattan_darkorange,
+                 grattantheme::grattan_red, grattantheme::grattan_darkred, "black",
+                 grattan_grey3)
+  } else if(n == 9){
+    palette <- c(grattantheme::grattan_lightyellow, grattantheme::grattan_yellow,
+                 grattantheme::grattan_lightorange, grattantheme::grattan_darkorange,
+                 grattantheme::grattan_red, grattantheme::grattan_darkred, "black",
+                 grattan_grey3, grattan_grey2)
+  } else if(n == 10){
+    palette <- c(grattantheme::grattan_lightyellow, grattantheme::grattan_yellow,
+                 grattantheme::grattan_lightorange, grattantheme::grattan_darkorange,
+                 grattantheme::grattan_red, grattantheme::grattan_darkred, "black",
+                 grattan_grey3, grattan_grey2, grattan_grey1)
   }
 
-if(reverse == TRUE){
-  rev(palette)
-} else {
-  palette
-}
+
+    if(reverse == TRUE){
+      rev(palette)
+    } else {
+      palette
+    }
 
 }
