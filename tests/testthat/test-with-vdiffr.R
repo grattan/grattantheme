92e2efa3e907a9654f48c4ed0da5a1674c9d1383
Matt Cowgill <mattcowgill@gmail.com>
2019-07-28 12:03:52 +1000

    abandoned attempt to test fullslide plots with vdiffr (font problem causing spurious mismatch)

--- tests/testthat/test-with-vdiffr.R
+++ tests/testthat/test-with-vdiffr.R
@@ -1,16 +1,16 @@
 
 context("vdiffr-tests")
 
-test_that("normal plot looks correct", {
+normal_plot <- mtcars %>%
+  ggplot(aes(x = wt,
+             y = mpg)) +
+  geom_point() +
+  grattantheme::theme_grattan() +
+  labs(title = "Here goes a Grattan title, blah blah lots of words go here extremely orange",
+       subtitle = "Either put units here or jam an elaborate thing here that describes both axes, whatevs",
+       caption = "Notes: Blah Source: somewhere")
 
-  normal_plot <- mtcars %>%
-    ggplot(aes(x = wt,
-               y = mpg)) +
-    geom_point() +
-    grattantheme::theme_grattan() +
-    labs(title = "Here goes a Grattan title, blah blah lots of words go here extremely orange",
-         subtitle = "Either put units here or jam an elaborate thing here that describes both axes, whatevs",
-         caption = "Notes: Blah Source: somewhere")
+test_that("normal plot looks correct", {
 
   vdiffr::expect_doppelganger("normal plot", normal_plot)
 
@@ -18,25 +18,14 @@ test_that("normal plot looks correct", {
 
 # test_that("fullslide plot looks correct", {
 #
-#   fullslide_plot <- function() {
-#
-#     normal_plot <- mtcars %>%
-#       ggplot(aes(x = wt,
-#                  y = mpg)) +
-#       geom_point() +
-#       grattantheme::theme_grattan() +
-#       labs(title = "Here goes a Grattan title, blah blah lots of words go here extremely orange",
-#            subtitle = "Either put units here or jam an elaborate thing here that describes both axes, whatevs",
-#            caption = "Notes: Blah Source: somewhere")
-#
-#     grattantheme:::create_fullslide(normal_plot, type = "fullslide", height = NULL, warn_labs = FALSE) %>%
+#   fullslide_plot <- grattantheme:::create_fullslide(normal_plot,
+#                                                     type = "fullslide",
+#                                                     height = NULL,
+#                                                     warn_labs = FALSE) %>%
 #       gridExtra::grid.arrange() %>%
-#       ggpubr::as_ggplot()
+#       ggplotify::as.ggplot()
 #
-#   }
 #
 #   vdiffr::expect_doppelganger("fullslide plot", fullslide_plot)
-#
-#
 # })
 #
