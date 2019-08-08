8fb124804f3c256032e97a32e27651fd88ff5f5f
Matt Cowgill <mattcowgill@gmail.com>
2019-08-08 15:17:42 +1000

    test animation on travis

--- tests/testthat/test-grattan_animation.R
+++ tests/testthat/test-grattan_animation.R
@@ -1,23 +1,26 @@
 library(ggplot2)
 library(gganimate)
 
-static_plot <- ggplot(mtcars,
-                      aes(x = wt, y = mpg)) +
-  geom_point() +
-  labs(title = "If you use a long title, grattan_anim_save() will break it over two lines (but not three)",
-       subtitle = "Your subtitle will also break over two lines if it needs to, and let's face it, it probably will need to. How nice! How cool!",
-       caption = "And the title, subtitle, and caption are all aligned with the left of the image, not the left of the plotting area.") +
-  theme_grattan()  +
-  theme(axis.title = element_blank())
-
-anim_plot <- static_plot +
-  transition_states(cyl)
-
 test_that("animation works with blog type chart", {
 
-  skip_on_travis()
+  #skip_on_travis()
 
-  grattan_anim_save("test.gif", anim_plot, type = "blog", nframes = 6, fps = 2)
+  static_plot <- ggplot(mtcars,
+                        aes(x = wt, y = mpg)) +
+    geom_point() +
+    labs(title = "If you use a long title, grattan_anim_save() will break it over two lines (but not three)",
+         subtitle = "Your subtitle will also break over two lines if it needs to, and let's face it, it probably will need to. How nice! How cool!",
+         caption = "And the title, subtitle, and caption are all aligned with the left of the image, not the left of the plotting area.") +
+    theme_grattan()  +
+    theme(axis.title = element_blank())
+
+  anim_plot <- static_plot +
+    transition_states(cyl)
+
+  tempfile <- tempfile()
+  on.exit(unlink(tempfile))
+
+  grattan_anim_save(tempfile, anim_plot, type = "blog", nframes = 6, fps = 2)
 
   expect_true(file.exists("test.gif"))
 
@@ -27,25 +30,5 @@ test_that("animation works with blog type chart", {
 
   unlink("test.gif")
 
-})
-
-test_that("animation works with normal type chart", {
-
-  skip_on_travis()
-
-  grattan_anim_save("test_normal.gif", anim_plot, type = "normal", nframes = 6, fps = 2)
-
-  expect_true(file.exists("test_normal.gif"))
-
-  expect_gt(file.size("test_normal.gif"), 180000)
-
-  expect_lt(file.size("test_normal.gif"), 250000)
-
-  unlink("test_normal.gif")
-
-})
-
-test_that("animation throws error when unsupported type is requested",{
-  expect_error(grattan_anim_save("test.gif", anim_plot, type = "fullslide_169", nframes = 6, fps = 2))
 })
 
