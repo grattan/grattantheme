84dc53d3c719b2fea37657864b5ba2fa43c9dd6e
Matt Cowgill <mattcowgill@gmail.com>
2020-09-29 14:44:05 +1000

    tests

--- tests/testthat/test-create_fullslide.R
+++ tests/testthat/test-create_fullslide.R
@@ -1,11 +1,11 @@
-test_that("create_fullslide works", {
-  p <- ggplot(mtcars, aes(x = wt, y = mpg)) +
-    geom_point() +
-    theme_grattan() +
-    labs(title = "Title goes here",
-         subtitle = "Subtitle here",
-         caption = "Notes: some notes. Source: source information")
+p <- ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() +
+  theme_grattan() +
+  labs(title = "Title goes here",
+       subtitle = "Subtitle here",
+       caption = "Notes: some notes. Source: source information")
 
+test_that("create_fullslide works", {
   expect_error(create_fullslide(p, "normal"))
 
   full_p <- create_fullslide(p, "fullslide")
@@ -14,4 +14,24 @@ test_that("create_fullslide works", {
   expect_length(full_p, 10)
   expect_false(is.null(full_p$patches))
 
+})
+
+test_that("saved fullslides look as expected", {
+  fullslide_types <- chart_types$type[chart_types$class == "fullslide"]
+  plots <- purrr::map(fullslide_types, create_fullslide, plot = p)
+  plots <- setNames(plots, fullslide_types)
+  filename <- file.path("test", "saved_plots.rda")
+
+  if (!dir.exists("test")) dir.create("test")
+
+  save(plots, file = filename)
+
+  expect_true(file.exists(filename))
+
+  saved_plots <- plots
+  load(filename)
+
+  expect_equal(saved_plots, plots)
+
+  all.equal(saved_plots, plots)
 })
