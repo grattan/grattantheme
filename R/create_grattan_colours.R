67ec07bf55d92e5f356557654223c445464f58ad
wfmackey <william.mackey@grattaninstitute.edu.au>
2021-09-21 14:35:13 +1000

    add colours

---
+++ R/create_grattan_colours.R
@@ -0,0 +1,307 @@
+# Define grattan colours =======================================================
+
+# Yellow light ----
+grattan_lightyellow <- "#ffe07f"
+  usethis::use_data(grattan_lightyellow, overwrite = TRUE)
+grattan_lightyellow1 <- "#ffe38c"
+  usethis::use_data(grattan_lightyellow1, overwrite = TRUE)
+grattan_lightyellow2 <- "#ffe699"
+  usethis::use_data(grattan_lightyellow2, overwrite = TRUE)
+grattan_lightyellow3 <- "#ffe9a5"
+  usethis::use_data(grattan_lightyellow3, overwrite = TRUE)
+grattan_lightyellow4 <- "#ffecb2"
+  usethis::use_data(grattan_lightyellow4, overwrite = TRUE)
+grattan_lightyellow5 <- "#fff0bf"
+  usethis::use_data(grattan_lightyellow5, overwrite = TRUE)
+grattan_lightyellow6 <- "#fff3cc"
+  usethis::use_data(grattan_lightyellow6, overwrite = TRUE)
+grattan_lightyellow7 <- "#fff6d9"
+  usethis::use_data(grattan_lightyellow7, overwrite = TRUE)
+grattan_lightyellow8 <- "#fff9e5"
+  usethis::use_data(grattan_lightyellow8, overwrite = TRUE)
+
+
+grattan_yellow <- grattan_lightyellow
+  usethis::use_data(grattan_yellow, overwrite = TRUE)
+grattan_yellow1 <- grattan_lightyellow1
+  usethis::use_data(grattan_yellow1, overwrite = TRUE)
+grattan_yellow2 <- grattan_lightyellow2
+  usethis::use_data(grattan_yellow2, overwrite = TRUE)
+grattan_yellow3 <- grattan_lightyellow3
+  usethis::use_data(grattan_yellow3, overwrite = TRUE)
+grattan_yellow4 <- grattan_lightyellow4
+  usethis::use_data(grattan_yellow4, overwrite = TRUE)
+grattan_yellow5 <- grattan_lightyellow5
+  usethis::use_data(grattan_yellow5, overwrite = TRUE)
+grattan_yellow6 <- grattan_lightyellow6
+  usethis::use_data(grattan_yellow6, overwrite = TRUE)
+grattan_yellow7 <- grattan_lightyellow7
+  usethis::use_data(grattan_yellow7, overwrite = TRUE)
+grattan_yellow8 <- grattan_lightyellow8
+  usethis::use_data(grattan_yellow8, overwrite = TRUE)
+
+
+# dark
+grattan_darkyellow <- "#ffc35a"  # grattan_yellow
+  usethis::use_data(grattan_darkyellow, overwrite = TRUE)
+grattan_darkyellow1 <- "#ffc96b"
+  usethis::use_data(grattan_darkyellow1, overwrite = TRUE)
+grattan_darkyellow2 <- "#ffcf7b"
+  usethis::use_data(grattan_darkyellow2, overwrite = TRUE)
+grattan_darkyellow3 <- "#ffd58c"
+  usethis::use_data(grattan_darkyellow3, overwrite = TRUE)
+grattan_darkyellow4 <- "#ffdb9c"
+  usethis::use_data(grattan_darkyellow4, overwrite = TRUE)
+grattan_darkyellow5 <- "#ffe1ad"
+  usethis::use_data(grattan_darkyellow5, overwrite = TRUE)
+grattan_darkyellow6 <- "#ffe7bd"
+  usethis::use_data(grattan_darkyellow6, overwrite = TRUE)
+grattan_darkyellow7 <- "#ffedce"
+  usethis::use_data(grattan_darkyellow7, overwrite = TRUE)
+grattan_darkyellow8 <- "#fff3de"
+  usethis::use_data(grattan_darkyellow8, overwrite = TRUE)
+
+grattan_yellow <- grattan_darkyellow
+  usethis::use_data(grattan_yellow, overwrite = TRUE)
+grattan_yellow1 <- grattan_darkyellow1
+  usethis::use_data(grattan_yellow1, overwrite = TRUE)
+grattan_yellow2 <- grattan_darkyellow2
+  usethis::use_data(grattan_yellow2, overwrite = TRUE)
+grattan_yellow3 <- grattan_darkyellow3
+  usethis::use_data(grattan_yellow3, overwrite = TRUE)
+grattan_yellow4 <- grattan_darkyellow4
+  usethis::use_data(grattan_yellow4, overwrite = TRUE)
+grattan_yellow5 <- grattan_darkyellow5
+  usethis::use_data(grattan_yellow5, overwrite = TRUE)
+grattan_yellow6 <- grattan_darkyellow6
+  usethis::use_data(grattan_yellow6, overwrite = TRUE)
+grattan_yellow7 <- grattan_darkyellow7
+  usethis::use_data(grattan_yellow7, overwrite = TRUE)
+grattan_yellow8 <- grattan_darkyellow8
+  usethis::use_data(grattan_yellow8, overwrite = TRUE)
+
+
+
+# Orange light ----------
+grattan_lightorange <- "#f68b33"  # grattan_light_orange
+  usethis::use_data(grattan_lightorange, overwrite = TRUE)
+grattan_lightorange1 <- "#f79747"
+  usethis::use_data(grattan_lightorange1, overwrite = TRUE)
+grattan_lightorange2 <- "#f8a25c"
+  usethis::use_data(grattan_lightorange2, overwrite = TRUE)
+grattan_lightorange3 <- "#f9ae70"
+  usethis::use_data(grattan_lightorange3, overwrite = TRUE)
+grattan_lightorange4 <- "#fab985"
+  usethis::use_data(grattan_lightorange4, overwrite = TRUE)
+grattan_lightorange5 <- "#fbc599"
+  usethis::use_data(grattan_lightorange5, overwrite = TRUE)
+grattan_lightorange6 <- "#fbd1ad"
+  usethis::use_data(grattan_lightorange6, overwrite = TRUE)
+grattan_lightorange7 <- "#fcdcc2"
+  usethis::use_data(grattan_lightorange7, overwrite = TRUE)
+grattan_lightorange8 <- "#fde8d6"
+  usethis::use_data(grattan_lightorange8, overwrite = TRUE)
+
+grattan_orangt <- grattan_lightorange
+  usethis::use_data(grattan_orangt, overwrite = TRUE)
+grattan_orange1 <- grattan_lightorange1
+  usethis::use_data(grattan_orange1, overwrite = TRUE)
+grattan_orange2 <- grattan_lightorange2
+  usethis::use_data(grattan_orange2, overwrite = TRUE)
+grattan_orange3 <- grattan_lightorange3
+  usethis::use_data(grattan_orange3, overwrite = TRUE)
+grattan_orange4 <- grattan_lightorange4
+  usethis::use_data(grattan_orange4, overwrite = TRUE)
+grattan_orange5 <- grattan_lightorange5
+  usethis::use_data(grattan_orange5, overwrite = TRUE)
+grattan_orange6 <- grattan_lightorange6
+  usethis::use_data(grattan_orange6, overwrite = TRUE)
+grattan_orange7 <- grattan_lightorange7
+  usethis::use_data(grattan_orange7, overwrite = TRUE)
+grattan_orange8 <- grattan_lightorange8
+  usethis::use_data(grattan_orange8, overwrite = TRUE)
+
+# dark:
+grattan_darkorange <- "#d4582a"
+  usethis::use_data(grattan_darkorange, overwrite = TRUE)
+grattan_darkorange1 <- "#d8693f"
+  usethis::use_data(grattan_darkorange1, overwrite = TRUE)
+grattan_darkorange2 <- "#dd7955"
+  usethis::use_data(grattan_darkorange2, overwrite = TRUE)
+grattan_darkorange3 <- "#e18a6a"
+  usethis::use_data(grattan_darkorange3, overwrite = TRUE)
+grattan_darkorange4 <- "#e59b7f"
+  usethis::use_data(grattan_darkorange4, overwrite = TRUE)
+grattan_darkorange5 <- "#eaac95"
+  usethis::use_data(grattan_darkorange5, overwrite = TRUE)
+grattan_darkorange6 <- "#eebcaa"
+  usethis::use_data(grattan_darkorange6, overwrite = TRUE)
+grattan_darkorange7 <- "#f2cdbf"
+  usethis::use_data(grattan_darkorange7, overwrite = TRUE)
+grattan_darkorange8 <- "#f6ded4"
+  usethis::use_data(grattan_darkorange8, overwrite = TRUE)
+
+
+# Red --------------
+# light:
+grattan_lightred <- "#a02226"
+  usethis::use_data(grattan_lightred, overwrite = TRUE)
+grattan_lightred1 <- "#aa383c"
+  usethis::use_data(grattan_lightred1, overwrite = TRUE)
+grattan_lightred2 <- "#b34e51"
+  usethis::use_data(grattan_lightred2, overwrite = TRUE)
+grattan_lightred3 <- "#bd6467"
+  usethis::use_data(grattan_lightred3, overwrite = TRUE)
+grattan_lightred4 <- "#c67a7d"
+  usethis::use_data(grattan_lightred4, overwrite = TRUE)
+grattan_lightred5 <- "#d09193"
+  usethis::use_data(grattan_lightred5, overwrite = TRUE)
+grattan_lightred6 <- "#d9a7a8"
+  usethis::use_data(grattan_lightred6, overwrite = TRUE)
+grattan_lightred7 <- "#e3bdbe"
+  usethis::use_data(grattan_lightred7, overwrite = TRUE)
+grattan_lightred8 <- "#ecd3d4"
+  usethis::use_data(grattan_lightred8, overwrite = TRUE)
+
+grattan_red <- grattan_lightred
+  usethis::use_data(grattan_red, overwrite = TRUE)
+grattan_red1 <- grattan_lightred1
+  usethis::use_data(grattan_red1, overwrite = TRUE)
+grattan_red2 <- grattan_lightred2
+  usethis::use_data(grattan_red2, overwrite = TRUE)
+grattan_red3 <- grattan_lightred3
+  usethis::use_data(grattan_red3, overwrite = TRUE)
+grattan_red4 <- grattan_lightred4
+  usethis::use_data(grattan_red4, overwrite = TRUE)
+grattan_red5 <- grattan_lightred5
+  usethis::use_data(grattan_red5, overwrite = TRUE)
+grattan_red6 <- grattan_lightred6
+  usethis::use_data(grattan_red6, overwrite = TRUE)
+grattan_red7 <- grattan_lightred7
+  usethis::use_data(grattan_red7, overwrite = TRUE)
+grattan_red8 <- grattan_lightred8
+  usethis::use_data(grattan_red8, overwrite = TRUE)
+
+# dark:
+grattan_darkred <- "#621214"
+  usethis::use_data(grattan_darkred, overwrite = TRUE)
+grattan_darkred1 <- "#722a2c"
+  usethis::use_data(grattan_darkred1, overwrite = TRUE)
+grattan_darkred2 <- "#814143"
+  usethis::use_data(grattan_darkred2, overwrite = TRUE)
+grattan_darkred3 <- "#91595b"
+  usethis::use_data(grattan_darkred3, overwrite = TRUE)
+grattan_darkred4 <- "#a17172"
+  usethis::use_data(grattan_darkred4, overwrite = TRUE)
+grattan_darkred5 <- "#b1898a"
+  usethis::use_data(grattan_darkred5, overwrite = TRUE)
+grattan_darkred6 <- "#c0a0a1"
+  usethis::use_data(grattan_darkred6, overwrite = TRUE)
+grattan_darkred7 <- "#d0b8b9"
+  usethis::use_data(grattan_darkred7, overwrite = TRUE)
+grattan_darkred8 <- "#e0d0d0"
+  usethis::use_data(grattan_darkred8, overwrite = TRUE)
+
+
+# Blue -------------------------------------------------------------------------
+# light:
+grattan_lightblue <- "#a3c7df"
+  usethis::use_data(grattan_lightblue, overwrite = TRUE)
+grattan_lightblue1 <- "#accde2"
+  usethis::use_data(grattan_lightblue1, overwrite = TRUE)
+grattan_lightblue2 <- "#b5d2e5"
+  usethis::use_data(grattan_lightblue2, overwrite = TRUE)
+grattan_lightblue3 <- "#bfd8e9"
+  usethis::use_data(grattan_lightblue3, overwrite = TRUE)
+grattan_lightblue4 <- "#c8ddec"
+  usethis::use_data(grattan_lightblue4, overwrite = TRUE)
+grattan_lightblue5 <- "#d1e3ef"
+  usethis::use_data(grattan_lightblue5, overwrite = TRUE)
+grattan_lightblue6 <- "#dae9f2"
+  usethis::use_data(grattan_lightblue6, overwrite = TRUE)
+grattan_lightblue7 <- "#e3eef5"
+  usethis::use_data(grattan_lightblue7, overwrite = TRUE)
+grattan_lightblue8 <- "#edf4f9"
+  usethis::use_data(grattan_lightblue8, overwrite = TRUE)
+
+grattan_blue <- grattan_lightblue
+  usethis::use_data(grattan_blue, overwrite = TRUE)
+grattan_blue1 <- grattan_lightblue1
+  usethis::use_data(grattan_blue1, overwrite = TRUE)
+grattan_blue2 <- grattan_lightblue2
+  usethis::use_data(grattan_blue2, overwrite = TRUE)
+grattan_blue3 <- grattan_lightblue3
+  usethis::use_data(grattan_blue3, overwrite = TRUE)
+grattan_blue4 <- grattan_lightblue4
+  usethis::use_data(grattan_blue4, overwrite = TRUE)
+grattan_blue5 <- grattan_lightblue5
+  usethis::use_data(grattan_blue5, overwrite = TRUE)
+grattan_blue6 <- grattan_lightblue6
+  usethis::use_data(grattan_blue6, overwrite = TRUE)
+grattan_blue7 <- grattan_lightblue7
+  usethis::use_data(grattan_blue7, overwrite = TRUE)
+grattan_blue8 <- grattan_lightblue8
+  usethis::use_data(grattan_blue8, overwrite = TRUE)
+
+# dark
+grattan_darkblue <- "#3e81ce"
+  usethis::use_data(grattan_darkblue, overwrite = TRUE)
+grattan_darkblue1 <- "#538fd4"
+  usethis::use_data(grattan_darkblue1, overwrite = TRUE)
+grattan_darkblue2 <- "#699dd9"
+  usethis::use_data(grattan_darkblue2, overwrite = TRUE)
+grattan_darkblue3 <- "#7eabdf"
+  usethis::use_data(grattan_darkblue3, overwrite = TRUE)
+grattan_darkblue4 <- "#94b9e4"
+  usethis::use_data(grattan_darkblue4, overwrite = TRUE)
+grattan_darkblue5 <- "#a9c7e9"
+  usethis::use_data(grattan_darkblue5, overwrite = TRUE)
+grattan_darkblue6 <- "#bfd5ef"
+  usethis::use_data(grattan_darkblue6, overwrite = TRUE)
+grattan_darkblue7 <- "#d4e3f4"
+  usethis::use_data(grattan_darkblue7, overwrite = TRUE)
+grattan_darkblue8 <- "#eaf1fa"
+  usethis::use_data(grattan_darkblue8, overwrite = TRUE)
+
+
+# grey ------
+
+# light
+grattan_lightgrey <- "#828282"
+  usethis::use_data(grattan_lightgrey, overwrite = TRUE)
+grattan_lightgrey1 <- "#8f8f8f"
+  usethis::use_data(grattan_lightgrey1, overwrite = TRUE)
+grattan_lightgrey2 <- "#9b9b9b"
+  usethis::use_data(grattan_lightgrey2, overwrite = TRUE)
+grattan_lightgrey3 <- "#a8a8a8"
+  usethis::use_data(grattan_lightgrey3, overwrite = TRUE)
+grattan_lightgrey4 <- "#b4b4b4"
+  usethis::use_data(grattan_lightgrey4, overwrite = TRUE)
+grattan_lightgrey5 <- "#c1c1c1"
+  usethis::use_data(grattan_lightgrey5, overwrite = TRUE)
+grattan_lightgrey6 <- "#cdcdcd"
+  usethis::use_data(grattan_lightgrey6, overwrite = TRUE)
+grattan_lightgrey7 <- "#dadada"
+  usethis::use_data(grattan_lightgrey7, overwrite = TRUE)
+grattan_lightgrey8 <- "#e6e6e6"
+  usethis::use_data(grattan_lightgrey8, overwrite = TRUE)
+
+# dark
+grattan_darkgrey <- "#575757"
+  usethis::use_data(grattan_darkgrey, overwrite = TRUE)
+grattan_darkgrey1 <- "#686868"
+  usethis::use_data(grattan_darkgrey1, overwrite = TRUE)
+grattan_darkgrey2 <- "#797979"
+  usethis::use_data(grattan_darkgrey2, overwrite = TRUE)
+grattan_darkgrey3 <- "#898989"
+  usethis::use_data(grattan_darkgrey3, overwrite = TRUE)
+grattan_darkgrey4 <- "#9a9a9a"
+  usethis::use_data(grattan_darkgrey4, overwrite = TRUE)
+grattan_darkgrey5 <- "#ababab"
+  usethis::use_data(grattan_darkgrey5, overwrite = TRUE)
+grattan_darkgrey6 <- "#bcbcbc"
+  usethis::use_data(grattan_darkgrey6, overwrite = TRUE)
+grattan_darkgrey7 <- "#cdcdcd"
+  usethis::use_data(grattan_darkgrey7, overwrite = TRUE)
+grattan_darkgrey8 <- "#dddddd"
+  usethis::use_data(grattan_darkgrey8, overwrite = TRUE)
