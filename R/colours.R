a9d77351abb222085afdd958525bee0d58f74271
wfmackey <william.mackey@grattaninstitute.edu.au>
2021-09-27 11:55:42 +1000

    add new colour documentation

--- R/colours.R
+++ R/colours.R
@@ -1,15 +1,759 @@
-#' Hex code for the colour: Grattan dark orange (carrot soup)
+#' Hex code for the colour: Grattan Light Yellow (corn chowder)
+#'
+#' #FFE07F
+#'
+"grattan_lightyellow"
+
+#' Hex code for the colour: Grattan Light Yellow [tint 1]
+#'
+#' #FFE38C
+#'
+"grattan_lightyellow1"
+
+#' Hex code for the colour: Grattan Light Yellow [tint 2]
+#'
+#' #FFE699
+#'
+"grattan_lightyellow2"
+
+#' Hex code for the colour: Grattan Light Yellow [tint 3]
+#'
+#' #FFE9A5
+#'
+"grattan_lightyellow3"
+
+#' Hex code for the colour: Grattan Light Yellow [tint 4]
+#'
+#' #FFECB2
+#'
+"grattan_lightyellow4"
+
+#' Hex code for the colour: Grattan Light Yellow [tint 5]
+#'
+#' #FFF0BF
+#'
+"grattan_lightyellow5"
+
+#' Hex code for the colour: Grattan Light Yellow [tint 6]
+#'
+#' #FFF3CC
+#'
+"grattan_lightyellow6"
+
+#' Hex code for the colour: Grattan Light Yellow [tint 7]
+#'
+#' #FFF6D9
+#'
+"grattan_lightyellow7"
+
+#' Hex code for the colour: Grattan Light Yellow [tint 8]
+#'
+#' #FFF9E5
+#'
+"grattan_lightyellow8"
+
+#' Hex code for the colour: Grattan Dark Yellow (butternut pumpkin soup)
+#'
+#' #FFC35A
+#'
+"grattan_darkyellow"
+
+#' Hex code for the colour: Grattan Dark Yellow [tint 1]
+#'
+#' #FFC96B
+#'
+"grattan_darkyellow1"
+
+#' Hex code for the colour: Grattan Dark Yellow [tint 2]
+#'
+#' #FFCF7B
+#'
+"grattan_darkyellow2"
+
+#' Hex code for the colour: Grattan Dark Yellow [tint 3]
+#'
+#' #FFD58C
+#'
+"grattan_darkyellow3"
+
+#' Hex code for the colour: Grattan Dark Yellow [tint 4]
+#'
+#' #FFDB9C
+#'
+"grattan_darkyellow4"
+
+#' Hex code for the colour: Grattan Dark Yellow [tint 5]
+#'
+#' #FFE1AD
+#'
+"grattan_darkyellow5"
+
+#' Hex code for the colour: Grattan Dark Yellow [tint 6]
+#'
+#' #FFE7BD
+#'
+"grattan_darkyellow6"
+
+#' Hex code for the colour: Grattan Dark Yellow [tint 7]
+#'
+#' #FFEDCE
+#'
+"grattan_darkyellow7"
+
+#' Hex code for the colour: Grattan Dark Yellow [tint 8]
+#'
+#' #FFF3DE
+#'
+"grattan_darkyellow8"
+
+#' Hex code for the colour: Grattan Light Orange (sweet potato soup)
+#'
+#' #F68B33
+#'
+"grattan_lightorange"
+
+#' Hex code for the colour: Grattan Light Orange [tint 1]
+#'
+#' #F79747
+#'
+"grattan_lightorange1"
+
+#' Hex code for the colour: Grattan Light Orange [tint 2]
+#'
+#' #F8A25C
+#'
+"grattan_lightorange2"
+
+#' Hex code for the colour: Grattan Light Orange [tint 3]
+#'
+#' #F9AE70
+#'
+"grattan_lightorange3"
+
+#' Hex code for the colour: Grattan Light Orange [tint 4]
+#'
+#' #FAB985
+#'
+"grattan_lightorange4"
+
+#' Hex code for the colour: Grattan Light Orange [tint 5]
+#'
+#' #FBC599
+#'
+"grattan_lightorange5"
+
+#' Hex code for the colour: Grattan Light Orange [tint 6]
+#'
+#' #FBD1AD
+#'
+"grattan_lightorange6"
+
+#' Hex code for the colour: Grattan Light Orange [tint 7]
+#'
+#' #FCDCC2
+#'
+"grattan_lightorange7"
+
+#' Hex code for the colour: Grattan Light Orange [tint 8]
+#'
+#' #FDE8D6
+#'
+"grattan_lightorange8"
+
+#' Hex code for the colour: Grattan Dark Orange (carrot soup)
 #'
 #' #D4582A
 #'
 "grattan_darkorange"
 
+#' Hex code for the colour: Grattan Dark Orange [tint 1]
+#'
+#' #D8693F
+#'
+"grattan_darkorange1"
 
-#' Hex code for the colour: Grattan dark red (kidney bean soup)
+#' Hex code for the colour: Grattan Dark Orange [tint 2]
+#'
+#' #DD7955
+#'
+"grattan_darkorange2"
+
+#' Hex code for the colour: Grattan Dark Orange [tint 3]
+#'
+#' #E18A6A
+#'
+"grattan_darkorange3"
+
+#' Hex code for the colour: Grattan Dark Orange [tint 4]
+#'
+#' #E59B7F
+#'
+"grattan_darkorange4"
+
+#' Hex code for the colour: Grattan Dark Orange [tint 5]
+#'
+#' #EAAC95
+#'
+"grattan_darkorange5"
+
+#' Hex code for the colour: Grattan Dark Orange [tint 6]
+#'
+#' #EEBCAA
+#'
+"grattan_darkorange6"
+
+#' Hex code for the colour: Grattan Dark Orange [tint 7]
+#'
+#' #F2CDBF
+#'
+"grattan_darkorange7"
+
+#' Hex code for the colour: Grattan Dark Orange [tint 8]
+#'
+#' #F6DED4
+#'
+"grattan_darkorange8"
+
+#' Hex code for the colour: Grattan Light Red (tomato soup)
+#'
+#' #A02226
+#'
+"grattan_lightred"
+
+#' Hex code for the colour: Grattan Light Red [tint 1]
+#'
+#' #AA383C
+#'
+"grattan_lightred1"
+
+#' Hex code for the colour: Grattan Light Red [tint 2]
+#'
+#' #B34E51
+#'
+"grattan_lightred2"
+
+#' Hex code for the colour: Grattan Light Red [tint 3]
+#'
+#' #BD6467
+#'
+"grattan_lightred3"
+
+#' Hex code for the colour: Grattan Light Red [tint 4]
+#'
+#' #C67A7D
+#'
+"grattan_lightred4"
+
+#' Hex code for the colour: Grattan Light Red [tint 5]
+#'
+#' #D09193
+#'
+"grattan_lightred5"
+
+#' Hex code for the colour: Grattan Light Red [tint 6]
+#'
+#' #D9A7A8
+#'
+"grattan_lightred6"
+
+#' Hex code for the colour: Grattan Light Red [tint 7]
+#'
+#' #E3BDBE
+#'
+"grattan_lightred7"
+
+#' Hex code for the colour: Grattan Light Red [tint 8]
+#'
+#' #ECD3D4
+#'
+"grattan_lightred8"
+
+#' Hex code for the colour: Grattan Dark Red (kidney bean soup)
 #'
 #' #621214
 #'
 "grattan_darkred"
+
+#' Hex code for the colour: Grattan Dark Red [tint 1]
+#'
+#' #722A2C
+#'
+"grattan_darkred1"
+
+#' Hex code for the colour: Grattan Dark Red [tint 2]
+#'
+#' #814143
+#'
+"grattan_darkred2"
+
+#' Hex code for the colour: Grattan Dark Red [tint 3]
+#'
+#' #91595B
+#'
+"grattan_darkred3"
+
+#' Hex code for the colour: Grattan Dark Red [tint 4]
+#'
+#' #A17172
+#'
+"grattan_darkred4"
+
+#' Hex code for the colour: Grattan Dark Red [tint 5]
+#'
+#' #B1898A
+#'
+"grattan_darkred5"
+
+#' Hex code for the colour: Grattan Dark Red [tint 6]
+#'
+#' #C0A0A1
+#'
+"grattan_darkred6"
+
+#' Hex code for the colour: Grattan Dark Red [tint 7]
+#'
+#' #D0B8B9
+#'
+"grattan_darkred7"
+
+#' Hex code for the colour: Grattan Dark Red [tint 8]
+#'
+#' #E0D0D0
+#'
+"grattan_darkred8"
+
+#' Hex code for the colour: Grattan Light Blue
+#'
+#' #A3C7DF
+#'
+"grattan_lightblue"
+
+#' Hex code for the colour: Grattan Light Blue [tint 1]
+#'
+#' #ACCDE2
+#'
+"grattan_lightblue1"
+
+#' Hex code for the colour: Grattan Light Blue [tint 2]
+#'
+#' #B5D2E5
+#'
+"grattan_lightblue2"
+
+#' Hex code for the colour: Grattan Light Blue [tint 3]
+#'
+#' #BFD8E9
+#'
+"grattan_lightblue3"
+
+#' Hex code for the colour: Grattan Light Blue [tint 4]
+#'
+#' #C8DDEC
+#'
+"grattan_lightblue4"
+
+#' Hex code for the colour: Grattan Light Blue [tint 5]
+#'
+#' #D1E3EF
+#'
+"grattan_lightblue5"
+
+#' Hex code for the colour: Grattan Light Blue [tint 6]
+#'
+#' #DAE9F2
+#'
+"grattan_lightblue6"
+
+#' Hex code for the colour: Grattan Light Blue [tint 7]
+#'
+#' #E3EEF5
+#'
+"grattan_lightblue7"
+
+#' Hex code for the colour: Grattan Light Blue [tint 8]
+#'
+#' #EDF4F9
+#'
+"grattan_lightblue8"
+
+#' Hex code for the colour: Grattan Dark Blue
+#'
+#' #3E81CE
+#'
+"grattan_darkblue"
+
+#' Hex code for the colour: Grattan Dark Blue [tint 1]
+#'
+#' #538FD4
+#'
+"grattan_darkblue1"
+
+#' Hex code for the colour: Grattan Dark Blue [tint 2]
+#'
+#' #699DD9
+#'
+"grattan_darkblue2"
+
+#' Hex code for the colour: Grattan Dark Blue [tint 3]
+#'
+#' #7EABDF
+#'
+"grattan_darkblue3"
+
+#' Hex code for the colour: Grattan Dark Blue [tint 4]
+#'
+#' #94B9E4
+#'
+"grattan_darkblue4"
+
+#' Hex code for the colour: Grattan Dark Blue [tint 5]
+#'
+#' #A9C7E9
+#'
+"grattan_darkblue5"
+
+#' Hex code for the colour: Grattan Dark Blue [tint 6]
+#'
+#' #BFD5EF
+#'
+"grattan_darkblue6"
+
+#' Hex code for the colour: Grattan Dark Blue [tint 7]
+#'
+#' #D4E3F4
+#'
+"grattan_darkblue7"
+
+#' Hex code for the colour: Grattan Dark Blue [tint 8]
+#'
+#' #EAF1FA
+#'
+"grattan_darkblue8"
+
+#' Hex code for the colour: Grattan Light Grey
+#'
+#' #828282
+#'
+"grattan_lightgrey"
+
+#' Hex code for the colour: Grattan Light Grey [tint 1]
+#'
+#' #8F8F8F
+#'
+"grattan_lightgrey1"
+
+#' Hex code for the colour: Grattan Light Grey [tint 2]
+#'
+#' #9B9B9B
+#'
+"grattan_lightgrey2"
+
+#' Hex code for the colour: Grattan Light Grey [tint 3]
+#'
+#' #A8A8A8
+#'
+"grattan_lightgrey3"
+
+#' Hex code for the colour: Grattan Light Grey [tint 4]
+#'
+#' #B4B4B4
+#'
+"grattan_lightgrey4"
+
+#' Hex code for the colour: Grattan Light Grey [tint 5]
+#'
+#' #C1C1C1
+#'
+"grattan_lightgrey5"
+
+#' Hex code for the colour: Grattan Light Grey [tint 6]
+#'
+#' #CDCDCD
+#'
+"grattan_lightgrey6"
+
+#' Hex code for the colour: Grattan Light Grey [tint 7]
+#'
+#' #DADADA
+#'
+"grattan_lightgrey7"
+
+#' Hex code for the colour: Grattan Light Grey [tint 8]
+#'
+#' #E6E6E6
+#'
+"grattan_lightgrey8"
+
+#' Hex code for the colour: Grattan Dark Grey
+#'
+#' #575757
+#'
+"grattan_darkgrey"
+
+#' Hex code for the colour: Grattan Dark Grey [tint 1]
+#'
+#' #686868
+#'
+"grattan_darkgrey1"
+
+#' Hex code for the colour: Grattan Dark Grey [tint 2]
+#'
+#' #797979
+#'
+"grattan_darkgrey2"
+
+#' Hex code for the colour: Grattan Dark Grey [tint 3]
+#'
+#' #898989
+#'
+"grattan_darkgrey3"
+
+#' Hex code for the colour: Grattan Dark Grey [tint 4]
+#'
+#' #9A9A9A
+#'
+"grattan_darkgrey4"
+
+#' Hex code for the colour: Grattan Dark Grey [tint 5]
+#'
+#' #ABABAB
+#'
+"grattan_darkgrey5"
+
+#' Hex code for the colour: Grattan Dark Grey [tint 6]
+#'
+#' #BCBCBC
+#'
+"grattan_darkgrey6"
+
+#' Hex code for the colour: Grattan Dark Grey [tint 7]
+#'
+#' #CDCDCD
+#'
+"grattan_darkgrey7"
+
+#' Hex code for the colour: Grattan Dark Grey [tint 8]
+#'
+#' #DDDDDD
+#'
+"grattan_darkgrey8"
+
+
+#' Hex code for the colour: Grattan Yellow (alias for Grattan Dark Yellow)
+#'
+#' #FFC35A
+#'
+"grattan_yellow"
+
+#' Hex code for the colour: Grattan Yellow [tint 1] (alias for Grattan Dark Yellow1)
+#'
+#' #FFC96B
+#'
+"grattan_yellow1"
+
+#' Hex code for the colour: Grattan Yellow [tint 2] (alias for Grattan Dark Yellow2)
+#'
+#' #FFCF7B
+#'
+"grattan_yellow2"
+
+#' Hex code for the colour: Grattan Yellow [tint 3] (alias for Grattan Dark Yellow3)
+#'
+#' #FFD58C
+#'
+"grattan_yellow3"
+
+#' Hex code for the colour: Grattan Yellow [tint 4] (alias for Grattan Dark Yellow4)
+#'
+#' #FFDB9C
+#'
+"grattan_yellow4"
+
+#' Hex code for the colour: Grattan Yellow [tint 5] (alias for Grattan Dark Yellow5)
+#'
+#' #FFE1AD
+#'
+"grattan_yellow5"
+
+#' Hex code for the colour: Grattan Yellow [tint 6] (alias for Grattan Dark Yellow6)
+#'
+#' #FFE7BD
+#'
+"grattan_yellow6"
+
+#' Hex code for the colour: Grattan Yellow [tint 7] (alias for Grattan Dark Yellow7)
+#'
+#' #FFEDCE
+#'
+"grattan_yellow7"
+
+#' Hex code for the colour: Grattan Yellow [tint 8] (alias for Grattan Dark Yellow8)
+#'
+#' #FFF3DE
+#'
+"grattan_yellow8"
+
+#' Hex code for the colour: Grattan Orange (alias for Grattan Light Orange)
+#'
+#' #F68B33
+#'
+"grattan_orange"
+
+#' Hex code for the colour: Grattan Orange [tint 1] (alias for Grattan Light Orange1)
+#'
+#' #F79747
+#'
+"grattan_orange1"
+
+#' Hex code for the colour: Grattan Orange [tint 2] (alias for Grattan Light Orange2)
+#'
+#' #F8A25C
+#'
+"grattan_orange2"
+
+#' Hex code for the colour: Grattan Orange [tint 3] (alias for Grattan Light Orange3)
+#'
+#' #F9AE70
+#'
+"grattan_orange3"
+
+#' Hex code for the colour: Grattan Orange [tint 4] (alias for Grattan Light Orange4)
+#'
+#' #FAB985
+#'
+"grattan_orange4"
+
+#' Hex code for the colour: Grattan Orange [tint 5] (alias for Grattan Light Orange5)
+#'
+#' #FBC599
+#'
+"grattan_orange5"
+
+#' Hex code for the colour: Grattan Orange [tint 6] (alias for Grattan Light Orange6)
+#'
+#' #FBD1AD
+#'
+"grattan_orange6"
+
+#' Hex code for the colour: Grattan Orange [tint 7] (alias for Grattan Light Orange7)
+#'
+#' #FCDCC2
+#'
+"grattan_orange7"
+
+#' Hex code for the colour: Grattan Orange [tint 8] (alias for Grattan Light Orange8)
+#'
+#' #FDE8D6
+#'
+"grattan_orange8"
+
+#' Hex code for the colour: Grattan Red (alias for Grattan Light Red)
+#'
+#' #A02226
+#'
+"grattan_red"
+
+#' Hex code for the colour: Grattan Red [tint 1] (alias for Grattan Light Red1)
+#'
+#' #AA383C
+#'
+"grattan_red1"
+
+#' Hex code for the colour: Grattan Red [tint 2] (alias for Grattan Light Red2)
+#'
+#' #B34E51
+#'
+"grattan_red2"
+
+#' Hex code for the colour: Grattan Red [tint 3] (alias for Grattan Light Red3)
+#'
+#' #BD6467
+#'
+"grattan_red3"
+
+#' Hex code for the colour: Grattan Red [tint 4] (alias for Grattan Light Red4)
+#'
+#' #C67A7D
+#'
+"grattan_red4"
+
+#' Hex code for the colour: Grattan Red [tint 5] (alias for Grattan Light Red5)
+#'
+#' #D09193
+#'
+"grattan_red5"
+
+#' Hex code for the colour: Grattan Red [tint 6] (alias for Grattan Light Red6)
+#'
+#' #D9A7A8
+#'
+"grattan_red6"
+
+#' Hex code for the colour: Grattan Red [tint 7] (alias for Grattan Light Red7)
+#'
+#' #E3BDBE
+#'
+"grattan_red7"
+
+#' Hex code for the colour: Grattan Red [tint 8] (alias for Grattan Light Red8)
+#'
+#' #ECD3D4
+#'
+"grattan_red8"
+
+#' Hex code for the colour: Grattan Blue (alias for Grattan Light Blue)
+#'
+#' #A3C7DF
+#'
+"grattan_blue"
+
+#' Hex code for the colour: Grattan Blue [tint 1] (alias for Grattan Light Blue1)
+#'
+#' #ACCDE2
+#'
+"grattan_blue1"
+
+#' Hex code for the colour: Grattan Blue [tint 2] (alias for Grattan Light Blue2)
+#'
+#' #B5D2E5
+#'
+"grattan_blue2"
+
+#' Hex code for the colour: Grattan Blue [tint 3] (alias for Grattan Light Blue3)
+#'
+#' #BFD8E9
+#'
+"grattan_blue3"
+
+#' Hex code for the colour: Grattan Blue [tint 4] (alias for Grattan Light Blue4)
+#'
+#' #C8DDEC
+#'
+"grattan_blue4"
+
+#' Hex code for the colour: Grattan Blue [tint 5] (alias for Grattan Light Blue5)
+#'
+#' #D1E3EF
+#'
+"grattan_blue5"
+
+#' Hex code for the colour: Grattan Blue [tint 6] (alias for Grattan Light Blue6)
+#'
+#' #DAE9F2
+#'
+"grattan_blue6"
+
+#' Hex code for the colour: Grattan Blue [tint 7] (alias for Grattan Light Blue7)
+#'
+#' #E3EEF5
+#'
+"grattan_blue7"
+
+#' Hex code for the colour: Grattan Blue [tint 8] (alias for Grattan Light Blue8)
+#'
+#' #EDF4F9
+#'
+"grattan_blue8"
 
 #' Hex code for the colour: Grattan grey 1 (truffle soup)
 #'
@@ -59,97 +803,17 @@
 #'
 "grattan_gridlinegrey"
 
-#' Hex code for the colour: Grattan light orange (sweet potato soup)
-#'
-#' #F68B33
-#'
-"grattan_lightorange"
-
-#' Hex code for the colour: Grattan light orange (sweet potato soup)
-#'
-#' #F68B33
-#'
-"grattan_orange"
-
-
-#' Hex code for the colour: Grattan light yello (corn chowder)
-#'
-#' #FFE07F
-#'
-"grattan_lightyellow"
-
 #' Hex code for the colour used for Grattan box backgrounds (orange alpha)
 #'
 #' #FEF0DE
 #'
 "grattan_orange_alpha"
 
-
-#' Hex code for the colour: Grattan red (tomato soup)
+#' Hex code for the colour used for Grattan box backgrounds (orange alpha)
 #'
-#' #A02226
+#' #FEF0DE
 #'
-"grattan_red"
-
-#' Hex code for the colour: Grattan yellow (butternut pumpkin soup)
-#'
-#' #FFC35A
-#'
-"grattan_yellow"
-
-
-#' Hex code for the faded colour: Grattan light yellow (faded)
-#'
-#' #FFE79F
-#'
-"grattan_lightyellow_f"
-
-
-
-#' Hex code for the faded colour: Grattan yellow (faded)
-#'
-#' #FFD283
-#'
-"grattan_yellow_f"
-
-
-
-#' Hex code for the faded colour: Grattan light orange (faded)
-#'
-#' #F8A866
-#'
-"grattan_lightorange_f"
-
-
-
-#' Hex code for the faded colour: Grattan light orange (faded)
-#'
-#' #F8A866
-#'
-"grattan_orange_f"
-
-
-
-#' Hex code for the faded colour: Grattan dark orange (faded)
-#'
-#' #DE815F
-#'
-"grattan_darkorange_f"
-
-
-#' Hex code for the faded colour: Grattan red (faded)
-#'
-#' #B7595C
-#'
-"grattan_red_f"
-
-
-#' Hex code for the faded colour: Grattan dark red (faded)
-#'
-#' #894D4E
-#'
-"grattan_darkred_f"
-
+"grattan_box"
 
 
 #' A list of Grattan colour sets
@@ -162,3 +826,56 @@
 #' "grey": grey 1, grey 2, grey 3, grey 4, grey 5
 #'
 "grattan_palette_set"
+
+
+#' !Legacy, depreciated!
+#' Hex code for the faded colour: Grattan light yellow (faded)
+#'
+#' #FFE79F
+#'
+"grattan_lightyellow_f"
+
+#' !Legacy, depreciated!
+#' Hex code for the faded colour: Grattan yellow (faded)
+#'
+#' #FFD283
+#'
+"grattan_yellow_f"
+
+#' !Legacy, depreciated!
+#' Hex code for the faded colour: Grattan light orange (faded)
+#'
+#' #F8A866
+#'
+"grattan_lightorange_f"
+
+#' !Legacy, depreciated!
+#' Hex code for the faded colour: Grattan light orange (faded)
+#'
+#' #F8A866
+#'
+"grattan_orange_f"
+
+
+#' !Legacy, depreciated!
+#' Hex code for the faded colour: Grattan dark orange (faded)
+#'
+#' #DE815F
+#'
+"grattan_darkorange_f"
+
+
+#' !Legacy, depreciated!
+#' Hex code for the faded colour: Grattan red (faded)
+#'
+#' #B7595C
+#'
+"grattan_red_f"
+
+
+#' !Legacy, depreciated!
+#' Hex code for the faded colour: Grattan dark red (faded)
+#'
+#' #894D4E
+#'
+"grattan_darkred_f"
