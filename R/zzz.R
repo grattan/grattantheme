a6d1f953ae3feedd87879936b5b2248ebd5094b1
raisin-toast <73819156+raisin-toast@users.noreply.github.com>
2022-12-29 12:50:03 +1100

    curse you windows

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,14 @@
 .onLoad <- function(libname, pkgname) {
 
-  cowsay::say(praise::praise(), sample(names(cowsay::animals), 1) )
+  cowsay::say(praise::praise(), sample(names(cowsay::animals)[!names(cowsay::animals) %in%  c("anxiouscat",
+                                                                                             "fish",
+                                                                                             "grumpycat",
+                                                                                             "longcat",
+                                                                                             "longtailcat",
+                                                                                             "mushroom",
+                                                                                             "shortcat",
+                                                                                             "signbunny",
+                                                                                             "stretchycat")], 1) )
   register_palette()
   set_aesthetics("grattan")
 
