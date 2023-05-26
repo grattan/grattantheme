e66fadb1d8dd7a2f08bde6f443aa84ce0dc826f7
raisin-toast <73819156+raisin-toast@users.noreply.github.com>
2023-05-26 17:12:49 +1000

    update tests and functions for new templates

--- data-raw/create sysdata.R
+++ data-raw/create sysdata.R
@@ -13,11 +13,12 @@ chart_types <- tibble::tribble(
                   "normal_169", "active",        30.00,   14.50,    180,     95,        100, "normal",    NA_character_,
                         "tiny", "active",        22.16,   11.08,    120,     70,         75, "normal",    NA_character_,
 
-                   "fullslide", "active",        33.87,   19.05,    175,     55,         95, "fullslide", "template_169.pptx",
+               "fullslide_old", "active",        33.87,   19.05,    175,     55,         95, "fullslide", "template_169.pptx",
                 "fullslide_43", "active",        25.40,   19.05,    140,     55,         70, "fullslide", "template_43.pptx",
-                        "blog", "active",        25.40,   19.05,    155,     62,         85, "fullslide", "template_blog.pptx",
+                        "blog", "deprecated",    25.40,   19.05,    155,     62,         85, "fullslide", "template_blog.pptx",
                           "a4", "active",        21.00,   29.70,    114,     66,         62, "fullslide", NA_character_,
-              "fullslide_2022", "active",        31.70,   19.05,    175,     55,         95, "fullslide", "template_2022.pptx",
+                   "fullslide", "active",        30.5,       14,    175,     55,         95, "fullslide", "template_fullslide.pptx",
+            "fullslide_narrow", "active",        23.0,       14,    175,     55,         95, "fullslide", "template_fullslide_narrow.pptx",
 
                 "fullslide_44", "deprecated",   25.40,   25.40,    140,     55,         95, "fullslide", NA_character_,
                    "blog_half", "deprecated",   25.4/2,  19.05,    155,     62,         85, "fullslide", "template_blog_half.pptx",
