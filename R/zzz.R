8f29b1c93d70e0f3147c16d9f17a45f7fac078c1
guslipkin <gus.lipkin@gmail.com>
2024-03-21 17:33:58 -0400

    Only cowsay when not interactive (and cleaned up)

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,14 @@
 .onLoad <- function(libname, pkgname) {
 
-  cowsay::say(praise::praise(), sample(names(cowsay::animals)[!names(cowsay::animals) %in%  c("anxiouscat",
-                                                                                             "fish",
-                                                                                             "grumpycat",
-                                                                                             "longcat",
-                                                                                             "longtailcat",
-                                                                                             "mushroom",
-                                                                                             "shortcat",
-                                                                                             "signbunny",
-                                                                                             "stretchycat")], 1) )
+  if (interactive()) {
+    unwanted_animals <- c(
+      "anxiouscat", "fish", "grumpycat", "longcat", "longtailcat", "mushroom",
+      "shortcat", "signbunny", "stretchycat"
+    )
+    wanted_animals <-
+      names(cowsay::animals)[!names(cowsay::animals) %in% unwanted_animals]
+    cowsay::say(praise::praise(), sample(wanted_animals, 1))
+  }
   register_palette()
   set_aesthetics("grattan")
 
