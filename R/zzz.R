a364428c596f8b91cc0a8fcfa5574ae207df4431
mattdbowes <matthew.bowes@grattaninstitute.edu.au>
2025-03-04 09:23:21 +1100

    Remove cowsay messages

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,5 @@
 .onLoad <- function(libname, pkgname) {
 
-if(interactive)  {cowsay::say(praise::praise(), sample(names(cowsay::animals)[!names(cowsay::animals) %in%  c("anxiouscat",
-                                                                                             "fish",
-                                                                                             "grumpycat",
-                                                                                             "longcat",
-                                                                                             "longtailcat",
-                                                                                             "mushroom",
-                                                                                             "shortcat",
-                                                                                             "signbunny",
-                                                                                             "stretchycat")], 1) )}
   register_palette()
   set_aesthetics("grattan")
 
