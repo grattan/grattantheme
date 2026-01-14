365903ced9884d8e92bd7925f5a1ecb9dbcd4f10
mattdbowes <matthew.bowes@grattaninstitute.edu.au>
2026-01-14 17:14:01 +1100

    Update dependencies
    Remove some unnecessary dependencies.

--- R/export_latex_code.R
+++ R/export_latex_code.R
@@ -19,7 +19,12 @@ export_latex_code <- function(p = ggplot2::last_plot(),
                               chart_path = "atlas/chart.pdf",
                               export_type = "wholecolumn") {
 
-  code_to_clipboard <- clipr::clipr_available()
+  if (!requireNamespace("clipr", quietly = TRUE)) {
+    message("Package 'clipr' is not installed. LaTeX code will be returned but not copied to clipboard.")
+    code_to_clipboard <- FALSE
+  } else {
+    code_to_clipboard <- clipr::clipr_available()
+  }
 
   # make export_path
   if (export_type != "") export_type <- paste0("_", export_type)
